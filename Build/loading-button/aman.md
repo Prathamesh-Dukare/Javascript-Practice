c2bbc416de1a2676d7a5e6101e5f6315724d72a8
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-07-30 15:35:29 +0530

    Update : Files

---
+++ Build/loading-button/aman.md
@@ -0,0 +1,33 @@
+Intro
+
+Semantic ✅
+Significance of meta tag ✅
+1. Can i add script tag at the top of body ✅ 
+2. what are the ways to add css to your html ✅
+3. Explain DOM tree✅
+
+
+Box model ✅
+Display None vs Visibility hidden 
+Relative and absolute units in css ✅
+What are various position properties ✅ 
+
+1. what is fetch in js?✅
+2. == vs === ✅
+3. js is async ? how 
+3. Arrow functons✅
+call back functions ✅
+4. What is promises in js?✅
+5. what if you want some operation to be done synchronously? ✅
+4. What if;
+   console.log(myFunc)	
+   let myFunc = ()=>{
+      return "hello"
+       } ✅
+5.Different client side storage✅
+
+
+
+Machine coding
+Show a load button screen, after clicking on the button the text loading should appear on screen for 3 secnods
+after 3 seconds they loading text should disappear and load button should again reapear on screen?
--- Build/loading-button/script.js
+++ Build/loading-button/script.js
@@ -1,11 +1,19 @@
-const loadingText = document.querySelector("#loading-text");
-const loadButton = document.querySelector("#load-btn");
+// const loadingText = document.querySelector("#loading-text");
+// const loadButton = document.querySelector("#load-btn");
 
-loadButton.addEventListener("click", function loadingHandler() {
-    loadingText.style.display = "block";
-    loadButton.style.display = "none";
-    setTimeout(()=>{
-        loadingText.style.display = "none";
-        loadButton.style.display = "block";
-    }, 3000);
-})
+// loadButton.addEventListener("click", function loadingHandler() {
+//     loadingText.style.display = "block";
+//     loadButton.style.display = "none";
+//     setTimeout(()=>{
+//         loadingText.style.display = "none";
+//         loadButton.style.display = "block";
+//     }, 3000);
+// })
+
+
+
+console.log(myVar)	
+let myVar = "hello"
+
+// console.log(message); //output : undefined
+// var message = "The variable Has been hoisted";
